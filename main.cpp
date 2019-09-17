f7c8669b1729e1a09805aaffdc09ce875b25c2fa
Radist <daniel.radist@gmail.com>
2019-09-17 23:25:39 +0700

    0.3 add 'Read'

--- main.cpp
+++ main.cpp
@@ -36,13 +36,30 @@ int Display(coordinates &kek)
 	cout << " seconds = " << kek.seconds << endl;
 }
 
+//Процедура ввода с клавиатуры
+int Read(coordinates &kek)
+{
+	cout << "Write degrees: ";
+	cin >> kek.degrees;
+
+	cout << "Write minutes: ";
+	cin >> kek.minutes;
+
+	cout << "Write seconds: ";
+	cin >> kek.seconds;
+}
+
 int main()
 {
-	coordinates one;
+	coordinates one, two;
 	Init(one);
+	cout << "Write two:" << endl;
+	Read(two);
 
 	cout << "One: ";
 	Display(one);
+	cout << "Two: ";
+	Display(two);
 
 	return 0;
 }
