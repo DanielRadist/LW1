d04983ad5cfdeb7e067bf6bde969369b8110e3b3
Radist <daniel.radist@gmail.com>
2019-09-17 23:03:39 +0700

    0.1 add 'Unit'

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/* @autor Rakhmanin Danila aka Radist AltSTU PI-82
+ *
+ * Разработать тип данных и набор процедур для работы с ним
+ * (не использовать конструкции ООП, желательно использовать язык С, использовать Git)
+ *
+ * 18. Класс координаты точки на карте состоит из трех вещественных полей (градусы, минуты и секунды).
+ * Реализовать метод округления до градусов и вычисления координат точки, находящейся на середине отрезка,
+ * соединяющего две другие точки (метод называется add).
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+struct coordinates
+{
+	float degrees;
+	float minutes;
+	float seconds;
+};
+
+//Процедура инициализации
+int Init(coordinates &kek)
+{
+	cout << "Init data." << endl;
+	kek.degrees = kek.minutes = kek.seconds = 0;
+	return 0;
+}
+
+int main()
+{
+	coordinates one, two;
+	Init(one);
+
+	return 0;
+}
