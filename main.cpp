3d24406e7efd331354e2f9f4a905305ab4131ff8
Radist <daniel.radist@gmail.com>
2019-09-23 07:30:26 +0700

    3.1 use OOP

--- main.cpp
+++ main.cpp
@@ -15,94 +15,85 @@
 
 using namespace std;
 
-struct food
+class food
 {
-	int taste;
-	int satiety;
-	int view;
-	int salt;
+private:
+	int taste, satiety, view, salt;
+public:
+	//Процедура инициализации
+	int Init()
+	{
+		cout << "Init food" << endl;
+		view = taste = satiety = salt = 0;
+		return 0;
+	}
+	//Процедура вывода на экран
+	int Display()
+	{
+		cout << "taste:" << taste;
+		cout << " satiety:" << satiety;
+		cout << " view:" << view;
+		cout << " salt:" << salt << endl;
+		return 0;
+	}
+	//Процедура ввода с клавиатуры
+	int Read()
+	{
+		cout << "taste: ";
+		cin >> taste;
+
+		cout << "satiety: ";
+		cin >> satiety;
+
+		cout << "view: ";
+		cin >> view;
+		return 0;
+	}
+	//объединить еду в блюдо
+	int Add(food &food1, food &food2)
+	{
+		//food1 + food2 = food3
+		taste = (food1.taste + food2.taste) / 2;
+		satiety = food1.satiety + food2.satiety;
+		view = (food1.view + food2.view) / 2;
+		salt = (food1.salt + food2.salt) / 2;
+		return 0;
+	}
+	//посолить еду
+	int toSalt()
+	{
+		salt += 1;
+
+		taste = taste / salt;
+	}
 };
-
-//Процедура инициализации
-int Init(food &food1)
-{
-	cout << "Init food" << endl;
-	food1.view = food1.taste = food1.satiety = food1.salt = 0;
-	return 0;
-}
-
-
-//Процедура вывода на экран
-int Display(food &food1)
-{
-	cout << "taste:" << food1.taste;
-	cout << " satiety:" << food1.satiety;
-	cout << " view:" << food1.view;
-	cout << " salt:" << food1.salt << endl;
-	return 0;
-}
-
-//Процедура ввода с клавиатуры
-int Read(food &food1)
-{
-	cout << "taste: ";
-	cin >> food1.taste;
-
-	cout << "satiety: ";
-	cin >> food1.satiety;
-
-	cout << "view: ";
-	cin >> food1.view;
-	return 0;
-}
-
-//объединить еду в блюдо
-int Add(food &food1, food &food2, food &food3)
-{
-	//food1 + food2 = food3
-	food3.taste = (food1.taste + food2.taste) / 2;
-	food3.satiety = food1.satiety + food2.satiety;
-	food3.view = (food1.view + food2.view) / 2;
-	food3.salt = (food1.salt + food2.salt) / 2;
-	return 0;
-}
-
-//посолить еду
-int toSalt(food &food1)
-{
-	food1.salt += 1;
-
-	food1.taste = food1.taste / food1.salt;
-}
-
 
 int main()
 {
 	food cutlet, potatoes, cutletAndPotatoes;
-	Init(cutlet);
-	Init(potatoes);
-	Init(cutletAndPotatoes);
+	cutlet.Init();
+	potatoes.Init();
+	cutletAndPotatoes.Init();
 
 	cout << "Cutlet: ";
-	Display(cutlet);
+	cutlet.Display();
 
 	cout << "Read cutlet: ";
-	Read(cutlet);
+	cutlet.Read();
 	cout << "Cutlet: ";
-	Display(cutlet);
+	cutlet.Display();
 
 	cout << "Read potatoes: ";
-	Read(potatoes);
+	potatoes.Read();
 
 	cout << "cutletAndPotatoes: ";
-	Add(cutlet, potatoes, cutletAndPotatoes);
-	toSalt(cutletAndPotatoes);
-	Display(cutletAndPotatoes);
+	cutletAndPotatoes.Add(cutlet, potatoes);
+	cutletAndPotatoes.toSalt();
+	cutletAndPotatoes.Display();
 
 	cout << "cutletAndPotatoes to salt: ";
-	toSalt(cutletAndPotatoes);
-	Display(cutletAndPotatoes);
-
+	cutletAndPotatoes.toSalt();
+	cutletAndPotatoes.Display();
 
 	return 0;
 }
